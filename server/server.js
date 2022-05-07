3472ac111c4b8157ea321bfc588e51a59f7883fd
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2022-05-07 12:42:06 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const Document = require("./Document")
 
-mongoose.connect("mongodb://localhost/google-docs-clone", {
+mongoose.connect("mongodb+srv://ZeroX:Balaram26@cluster0.b2lzi.mongodb.net/Roast-API?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
