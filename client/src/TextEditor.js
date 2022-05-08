b6b91521a23e6d761ac7a307b2af34c333ba9fce
DudeUnoob <to.baladev@gmail.com>
2022-05-07 20:47:28 -0500

    update

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    const s = io("https://fuzzy-tub-production.up.railway.app")
+    const s = io("http://localhost:3001")
     setSocket(s)
 
     return () => {
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ mongoose.connect("mongodb+srv://ZeroX:Balaram26@cluster0.b2lzi.mongodb.net/Roast
 
 const io = require("socket.io")(3001, {
   cors: {
-    origin: "https://google-docs-production.up.railway.app",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 })
